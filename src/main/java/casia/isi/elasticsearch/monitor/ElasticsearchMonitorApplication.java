6c5d394d878f0d3bb9cbad6e3b5c8ac199021c08
crazyyanchao <1141679423@qq.com>
2019-11-15 14:26:40 +0800

    Initial commit

---
+++ src/main/java/casia/isi/elasticsearch/monitor/ElasticsearchMonitorApplication.java
@@ -0,0 +1,13 @@
+package casia.isi.elasticsearch.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/casia/isi/elasticsearch/monitor/ElasticsearchMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package casia.isi.elasticsearch.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
